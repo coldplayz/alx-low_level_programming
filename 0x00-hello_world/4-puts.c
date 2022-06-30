30da225eed626f1da543f8764e355729741735f1
greenbel <greenbel.chibuike@gmail.com>
2022-06-30 18:39:29 +0100

    Commit 4.4. Added documentation in code.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - executes functions in the program
+ *
+ * return: returns integer zero to signify end of program
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
