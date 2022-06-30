f09b980bb42dcf0d21b341e95e6f8ecee84a8e73
greenbel <greenbel.chibuike@gmail.com>
2022-06-30 20:07:22 +0100

    Commit 6.2. Edited script.

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,6 +13,6 @@ int main(void)
 	long long int lli = sizeof(long long int);
 	float tf = sizeof(float);
 
-	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %.0f byte(s)\n", c, i, li, lli, tf);
+	printf("Size of a char: %d byte(s)\nSize of an int: %d byte(s)\nSize of a long int: %d byte(s)\nSize of a long long int: %d byte(s)\nSize of a float: %zu byte(s)\n", c, i, li, lli, tf);
 	return (0);
 }
