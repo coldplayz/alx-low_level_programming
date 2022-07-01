5f7bfdea622756aca9e6971bc8ec0841b9b0dc9f
greenbel <greenbel.chibuike@gmail.com>
2022-07-01 15:59:18 +0100

    Commit 1. Script 0-positive_or_negative.c, and README creation.

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	char description;
+	if(n < 0)
+	{
+		description = "is positive";
+	}
+	else if(n = 0)
+	{
+		description = "is zero";
+	}
+	else
+	{
+		description = "is negative";
+	}
+
+	printf("%d %c\n", n, description)
+	return (0);
+}
