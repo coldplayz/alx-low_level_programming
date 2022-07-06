e0da8de5eeb185cb9bb5c1ada55db51e51b5f373
greenbel <greenbel.chibuike@gmail.com>
2022-07-06 08:42:25 +0100

    Commit 7.3. modified:   7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,7 +8,7 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)                                        {                                                         n *= -1;
+	if (n < 0)                                        {
 		_putchar(((n % 10) * (-1)) + '0');
 		return (((n % 10) * (-1)));
         }
