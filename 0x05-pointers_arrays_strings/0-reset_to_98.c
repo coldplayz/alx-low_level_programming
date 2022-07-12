bf7eac2d382736218e89581cfe3adb3fee39b0d1
greenbel <greenbel.chibuike@gmail.com>
2022-07-12 16:37:24 +0000

    Commit 0.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - resets a number to 98
+ * @n: a pointer to an integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+reset_to_98.c
+* reset any number to 98
+
