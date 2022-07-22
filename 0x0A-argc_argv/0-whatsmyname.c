4a1d5f16dc1c394b7af9ffd1e5e5b2d4f8236bad
greenbel <greenbel.chibuike@gmail.com>
2022-07-22 15:06:26 +0000

    Commit 0.1
    modified:   0-whatsmyname.c
    new file:   a.out

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 
 /**
- * main -
+ * main - prints the name of the program
  * @argc: an integer representing the number of arguments passed to main
  * @argv: an array of pointers (pointer-to-pointer) to the string
  * representation of the command line arguments
  *
  * Return: always 0
  */
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
