d5ce3a5d52e96df5f5a891d3bd5a570e4b6959b9
greenbel <greenbel.chibuike@gmail.com>
2022-07-28 14:19:49 +0000

    Commit 2.3
    modified:   2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -27,7 +27,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	for (i = 0; i < (nmemb * size); i++)
 	{
-		*((int *)ptv + i) = 0;
+		*((char *)ptv + i) = 0;
 	}
 	/*ptv = (void *)pti;*/
 
