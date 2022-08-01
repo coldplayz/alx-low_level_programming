2b39a20fe73e165be233dc943bd706dd5f078199
greenbel <greenbel.chibuike@gmail.com>
2022-08-01 18:51:38 +0000

    Commit 1
           new file:   1-init_dog.c
           new file:   1-main.c
           modified:   README.md
           modified:   dog.h
           new file:   main.h

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - initializes a variable of type 'struct dog'
+ * @d: a pointer to the structure
+ * @name: a pointer to the dog name string
+ * @age: the dog's age as float
+ * @owner: a pointer to the owner name string
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/README.md
+++ 0x0E-structures_typedef/README.md
@@ -1,4 +1,7 @@
 dog.h
 * defines struct dog with fields: name, age, and owner
 
+1-init_dog.c
+* initializes a variable of type _struct dog_ 
+
 
