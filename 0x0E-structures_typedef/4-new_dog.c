ab7cf698d44ac0e3b176f15903e400c02f26a9ae
greenbel <greenbel.chibuike@gmail.com>
2022-08-02 06:47:35 +0000

    Commit 4.1
           modified:   4-new_dog.c
           modified:   dog.h
           modified:   z

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,14 +20,66 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	/*ptr->name = malloc(sizeof(char *));*/
-	ptr->name = name;
+	ptr->name = strdup2(name);
 
 	ptr->age = age;
 
-	/*ptr->owner = malloc(sizeof(char *));*/
-
-	ptr->owner = owner;
+	ptr->owner = strdup2(owner);
 
 	return (ptr);
+}
+
+
+/**
+ * strdup2 - copies a string to newly allocated memory.
+ * @str: pointer string to make a duplicate of.
+ *
+ * Return: a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ */
+char *strdup2(char *str)
+{
+	int i, j;
+	char *ptc;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	j = _strlen(str);
+	ptc = malloc((j + 1) * sizeof(char));
+	if (ptc == NULL)
+	{
+		return (NULL);
+	}
+
+	/*copy all the xters of str, including '\0'*/
+	for (i = 0; i <= j; i++)
+	{
+		*(ptc + i) = *(str + i);
+	}
+
+	return (ptc);
+}
+
+
+
+/**
+ * _strlen - returns the length of a string
+ * @s: pointer to char type
+ *
+ * Return: an integer length of the argument
+ */
+int _strlen(char *s)
+{
+	int len, shift = 0;
+
+	for (len = 0; *(s + shift);)
+	{
+		len++;
+		shift++;
+	}
+
+	return (len);
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,5 +18,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *strdup2(char *str);
+int _strlen(char *s);
+
 
 #endif /*dog_h*/
