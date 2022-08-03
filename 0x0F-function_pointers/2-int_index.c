344ed0a6c0563f672a726397e43b5f5519070c86
greenbel <greenbel.chibuike@gmail.com>
2022-08-03 13:15:37 +0000

    Commit 2
           new file:   2-int_index.c
           new file:   2-main.c
           modified:   README.md
           modified:   a.out
           modified:   function_pointers.h
           modified:   htemplate

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+#define IFNULL(ptf, ptc) (((ptf) == NULL) || ((ptc) == NULL))
+#define EXIT() exit(EXIT_SUCCESS)
+
+/**
+ * int_index - searches for an integer
+ * @array: the array to search in
+ * @size: the array size
+ * @cmp: a pointer to the callback to compare with
+ *
+ * Return: the index of the first element for which the
+ * cmp function does not return 0, or -1 if size <= 0 and/or no matches
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (IFNULL(cmp, array))
+	{
+		EXIT();
+	}
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -4,4 +4,8 @@
 1-array_iterator.c
 * a function that executes a callback function
 
+2-int_index.c
+* searches for an integer
+
+
 
