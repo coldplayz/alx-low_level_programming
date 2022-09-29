63bfb262e22d23415871c2cb565d6983ec84fff9
greenbel <greenbel.chibuike@gmail.com>
2022-09-29 15:06:51 +0000

    T5-0x17: minor issue fix.
    modified:   5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include "lists.h"
 
+size_t dlistint_len2(const dlistint_t *h);
+
+
 /**
  * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list.
  * @head: the first node
@@ -15,7 +18,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *nth_node, *temp;
 
 	temp = head;
-	list_len = dlistint_len(head);
+	list_len = dlistint_len2(head);
 	if (index >= list_len)
 	{
 		return (NULL);
@@ -28,4 +31,26 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	}
 
 	return (nth_node);
+}
+
+
+
+/**
+ * dlistint_len2 - determines the number of elements of the structure dlistint_s
+ * @h: the head of a structure of type 'struct dlistint_s'
+ *
+ * Return: the number of nodes in the list
+ */
+size_t dlistint_len2(const dlistint_t *h)
+{
+	size_t n = 0;
+	dlistint_t *temp;
+
+	temp = (dlistint_t *)h;
+	for (; temp; temp = temp->next)
+	{
+		n++;
+	}
+
+	return (n);
 }
