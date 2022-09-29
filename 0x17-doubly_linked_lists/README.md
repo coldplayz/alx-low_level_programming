bb11bb29e675d8a6d4f954520a17bfa3a4989865
greenbel <greenbel.chibuike@gmail.com>
2022-09-29 14:17:02 +0000

    T2-0x17: Initial commit.
           new file:   2-add_dnodeint.c
           new file:   2-main.c
           modified:   README.md
           modified:   lists.h

--- 0x17-doubly_linked_lists/README.md
+++ 0x17-doubly_linked_lists/README.md
@@ -4,4 +4,6 @@
 
 * 1-dlistint_len.c: returns the number of elements in a linked dlistint_t list.
 
+* 2-add_dnodeint.c: adds a new node at the beginning of a dlistint_t list.
+
 
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -113,7 +113,7 @@ int is_palindrome(listint_t **head);
 /* Doubly linked list */
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
-
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
 
 
 
