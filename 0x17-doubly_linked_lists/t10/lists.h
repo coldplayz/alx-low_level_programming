6b141782fed84b39d5b4ad873955707f10ff5308
greenbel <greenbel.chibuike@gmail.com>
2022-09-29 22:49:24 +0100

    T9 and T10 of 0x17.

--- 0x17-doubly_linked_lists/t10/lists.h
+++ 0x17-doubly_linked_lists/t10/lists.h
@@ -113,6 +113,13 @@ int is_palindrome(listint_t **head);
 
 /* string operations */
 int str_palindrome(char *str);
+void rev_string(char *s);
+
+
+/* integeral operations */
+char *itoa3(long int n);
+long int num_len2(long int n);
+
 
 
 /* Doubly linked list */
